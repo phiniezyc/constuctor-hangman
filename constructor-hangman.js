7b9a7a6fb7c6856ae4b08a7b30524bc385d25684
Chance Phiniezy <phiniezyc@gmail.com>
2017-10-25 19:44:38 -0400

    added wordsToGuess module

---
+++ constructor-hangman.js
@@ -0,0 +1,1 @@
+var wordsToGuess = require ("./wordsToGuess.js");
---
+++ wordsToGuess.js
@@ -0,0 +1,24 @@
+wordsToGuess = {
+    wordsToGuess: ["Beyonce", "Prince", "Sting", "Madonna", "Drake", "Elvis", "Elton", "Janis"]
+
+};
+
+
+
+
+
+console.log(wordsToGuess.wordsToGuess);
+
+
+
+
+
+
+
+
+
+
+
+module.export = {
+    wordsToGuess: wordsToGuess
+};
