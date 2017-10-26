1e009c9a6e9642140bb1c5f34cbaf5ae227112f5
Chance Phiniezy <phiniezyc@gmail.com>
2017-10-26 02:32:51 -0400

    split word into an array

--- wordsToGuess.js
+++ wordsToGuess.js
@@ -1,13 +1,24 @@
 var wordsToGuess = {
-    words: ["Beyonce", "Prince", "Sting", "Madonna", "Drake", "Elvis", "Elton", "Janis"]
+    words: ["beyonce", "prince", "sting", "madonna", "drake", "elvis", "elton", "janis"]
 
 };
 
 
 
 var wordForUserToGuess = wordsToGuess.words[Math.floor(Math.random() * wordsToGuess.words.length)];
-
 console.log(wordForUserToGuess);
+
+var wordSplitIntoArray = wordForUserToGuess.split("");
+console.log(wordSplitIntoArray);
+
+// function showLetterSpaces(word) {
+//     for (var i = 0; i < word.length; i++) {
+//         //[i]+ " ";
+
+//     }
+
+// }
+
 
 //console.log(wordsToGuess.words);
 
