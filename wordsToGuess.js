43784d033db281d18316fb8b467efa048054d6a0
Chance Phiniezy <phiniezyc@gmail.com>
2017-10-26 02:18:35 -0400

    Random word from word choice array is selected and set to a variable

--- wordsToGuess.js
+++ wordsToGuess.js
@@ -1,11 +1,13 @@
-wordsToGuess = {
+var wordsToGuess = {
     words: ["Beyonce", "Prince", "Sting", "Madonna", "Drake", "Elvis", "Elton", "Janis"]
 
 };
 
 
 
+var wordForUserToGuess = wordsToGuess.words[Math.floor(Math.random() * wordsToGuess.words.length)];
 
+console.log(wordForUserToGuess);
 
 //console.log(wordsToGuess.words);
 
