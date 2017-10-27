30722a5bf5c07d6806967b5199144398152818de
Chance Phiniezy <phiniezyc@gmail.com>
2017-10-27 01:53:25 -0400

    added word object, but might be useless

--- wordsToGuess.js
+++ wordsToGuess.js
@@ -1,27 +1,60 @@
-var wordsToGuess = {
-    words: ["beyonce", "prince", "sting", "madonna", "drake", "elvis", "elton", "janis"]
+var collectionOfWords = {
+    words: ["beyonce", "prince", "sting", "madonna", "drake", "elvis", "elton", "janis"],
+
 
 };
 
 
 
-var wordForUserToGuess = wordsToGuess.words[Math.floor(Math.random() * wordsToGuess.words.length)];
-console.log(wordForUserToGuess);
+
+
+var Word = {
+    collectionOfWords: ["beyonce", "prince", "sting", "madonna", "drake", "elvis", "elton", "janis"],
+
+    wordForUserToGuess: collectionOfWords.words[Math.floor(Math.random() * collectionOfWords.words.length)],
+
+    showWordForUserToGuess: function() {
+        console.log(this.wordForUserToGuess);
+    },
+
+    wordSplitIntoArray: function() {
+        console.log(this.wordForUserToGuess.split(""));
+    },
+    letterSpaces: [],
+
+    showLetterSpaces: function() {
+        for (var i = 0; i < this.wordSplitIntoArray.length; i++) {
+            this.letterSpaces = [];
+            this.letterSpaces.push("_");
+        }
+        console.log(this.letterSpaces);
+
+    }
+
+};
+
+Word.wordSplitIntoArray();
+Word.showLetterSpaces();
+
+
+var wordForUserToGuess = collectionOfWords.words[Math.floor(Math.random() * collectionOfWords.words.length)];
+//console.log(wordForUserToGuess);
 
 var wordSplitIntoArray = wordForUserToGuess.split("");
-console.log(wordSplitIntoArray);
+//console.log(wordSplitIntoArray);
 
 var letterSpaces = [];
-function showLetterSpaces(word) {
+function showLetterSpaces() {
     for (var i = 0; i < wordSplitIntoArray.length; i++) {
         letterSpaces.push("_");
     }
 
 }
-showLetterSpaces();
+// showLetterSpaces();
+// console.log(letterSpaces);
 
 //show the letter spaces w/ a space in between each space
-console.log(letterSpaces.join(" "));
+//console.log(letterSpaces.join(" "));
 
 
 //console.log(wordsToGuess.words);
@@ -37,5 +70,5 @@ console.log(letterSpaces.join(" "));
 
 
 module.export = {
-    wordsToGuess: wordsToGuess
+    wordsToGuess: collectionOfWords
 };
